6284e8c31d059d9e353810e7e42dd17ccfe4bde4
Adeniyi Olaitan <adeniyi.olaitanhector@yahoo.com>
2021-01-27 23:19:55 +0100

    updated settings, views.py, added css files annd html file, static folder created

--- src/PrEc/views.py
+++ src/PrEc/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('<h1>Hello World ! Sujay </h1>')
+    return render(request, "index.html")
